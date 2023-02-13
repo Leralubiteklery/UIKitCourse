b5171c8d8f7b580cf0a58cf35eb4eae42fbc42f7
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-02-13 18:58:35 +0700

    Added alert controller for wrong input format

--- UIKitCourse/ViewController.swift
+++ UIKitCourse/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var doneButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.layer.cornerRadius = 10
+        doneButton.layer.cornerRadius = 10
+    }
+    
+    private func presentAlert() {
+        let alert = UIAlertController(title: "Wrong format", message: "Please, enter your name", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
     }
 
-
+    @IBAction func buttonTapped() {
+        guard textField.text?.isEmpty == false else { return }
+        
+        if let _ = Double(textField.text!) {
+            presentAlert()
+        } else {
+            nameLabel.text = textField.text
+        }
+        
+    }
+    
 }
 
